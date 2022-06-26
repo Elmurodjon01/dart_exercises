da9eac11f6844dea1922c48297e26cf48adca1e4
Elmurodjon <96878908+Elmurodjon01@users.noreply.github.com>
2022-06-26 16:18:50 +0900

    pushing all files

--- lib/flutter/pageView_pageController.dart
+++ lib/flutter/pageView_pageController.dart
@@ -10,66 +10,137 @@ class MyApp extends StatefulWidget {
 }
 
 PageController pageController = PageController(initialPage: 0);
+int pageOrder = 0;
 
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView(
-                controller: pageController,
-                children: [
-                  Container(
-                    child: Image.asset(
-                      'assets/img1.jpeg',
-                      fit: BoxFit.cover,
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  onPageChanged: (pageindex) {
+                    setState(() {
+                      pageOrder = pageindex;
+                    });
+                  },
+                  controller: pageController,
+                  children: [
+                    Container(
+                      child: Image.asset(
+                        'assets/img1.jpeg',
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/img2.jpeg',
-                      fit: BoxFit.cover,
+                    Container(
+                      child: Image.asset(
+                        'assets/img2.jpeg',
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/img3.jpeg',
-                      fit: BoxFit.cover,
-                    ),
-                  )
-                ],
+                    Container(
+                      child: Image.asset(
+                        'assets/img3.jpeg',
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-              child: Image.asset('assets/middle.jpeg'),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    child: const Text(
-                      'Skip',
-                      style: TextStyle(color: Colors.black26),
-                    ),
-                    onPressed: () {},
-                  ),
-                  Divider(),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Next',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ],
+              Container(
+                child: Image.asset('assets/middle.jpeg'),
               ),
-            ),
-          ],
+              Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+                child: pageOrder == 2
+                    ? Container(
+                        child: TextButton(
+                          child: const Text(
+                            'Start',
+                            style: TextStyle(
+                                color: Colors.blueAccent, fontSize: 18),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MainScreen()));
+                          },
+                        ),
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            child: const Text(
+                              'Skip',
+                              style: TextStyle(
+                                color: Colors.black26,
+                                fontSize: 16,
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MainScreen()));
+                            },
+                          ),
+                          Row(
+                            children: List.generate(
+                              3,
+                              (dotIndex) {
+                                return Container(
+                                  margin: const EdgeInsets.all(5),
+                                  width: 12,
+                                  height: 12,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: pageOrder == dotIndex
+                                        ? Colors.blueAccent
+                                        : const Color(0xffcff1fb),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              pageController.animateToPage(pageOrder++,
+                                  duration: const Duration(milliseconds: 300),
+                                  curve: Curves.linear);
+                            },
+                            child: const Text(
+                              'Next',
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('welcome to main page'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hanpass',
+          style: TextStyle(fontSize: 40),
         ),
       ),
     );
